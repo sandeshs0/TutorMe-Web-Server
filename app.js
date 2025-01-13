125497843d2e02bcd7c2d1e6cae8038ead9301c0
sandeshs0 <hellosandesh0@gmail.com>
2025-01-13 20:50:29 +0545

    13- removed enrolled courses

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors=require("cors")
 const connectDb=require("./config/db")
 const UserRoute=require("./routes/userRoute")
 const TutorRoute=require("./routes/tutorRoute")
@@ -8,10 +9,17 @@ const app= express();
 
 connectDb();
 
+app.use(cors({
+    origin: "http://localhost:5173", // Frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    credentials: true, // Enable cookies if needed
+}));
+
 app.use(express.json());
 app.use("/api/users",UserRoute );
 app.use("/api/tutors",TutorRoute );
 app.use("/auth",AuthRoute);
+
 
 
 // app.use("/",()=>{
--- model/student.js
+++ model/student.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }], // Example attribute
     walletBalance: { type: Number, default: 0 }, // Funds for booking sessions
     dateJoined: { type: Date, default: Date.now },
 });
