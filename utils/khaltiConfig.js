a16c71803d451e5ada6fc2f4ad8d3e7e1df38c70
sandeshs0 <hellosandesh0@gmail.com>
2025-01-28 19:44:06 +0545

    25- khalti integration setup

---
+++ utils/khaltiConfig.js
@@ -0,0 +1,20 @@
+const khaltiConfig = {
+    publicKey: process.env.KHALTI_PUBLIC_KEY,
+    productIdentity: "student_wallet_load",
+    productName: "Wallet Load",
+    eventHandler: {
+      onSuccess(payload) {
+        // Call the verify transaction endpoint
+        console.log("Payment Success:", payload);
+      },
+      onError(error) {
+        console.error("Payment Error:", error);
+      },
+      onClose() {
+        console.log("Payment widget closed.");
+      },
+    },
+  };
+  
+  export default khaltiConfig;
+  
