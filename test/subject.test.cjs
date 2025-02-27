ddebc835f2637bde21970d2b15fe78121c6520ff
sandeshs0 <hellosandesh0@gmail.com>
2025-02-27 16:37:28 +0545

    api testing for subject

---
+++ test/subject.test.cjs
@@ -0,0 +1,21 @@
+const { app } = require("../app.js"); // ✅ Use only `app`, not `server`
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const mongoose = require("mongoose");
+const Subject = require("../model/subject");
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe("Subject API", () => {
+  it("should fetch all subjects", (done) => {
+    chai
+      .request(app)
+      .get("/api/subjects/getAll")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("array");
+        done();
+      });
+  });
+});
